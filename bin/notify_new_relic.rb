6f7bea3280225c6d4c88db786169d4af86f2889e
carllopez <carl.lopez@gmail.com>
2017-07-10 13:39:58 -0500

    Switching back to api key

--- bin/notify_new_relic.rb
+++ bin/notify_new_relic.rb
@@ -3,7 +3,7 @@
 require 'json'
 
 env_dir = ENV['ENV_DIR']
-new_relic_api_key = `cat #{env_dir}/NEW_RELIC_LICENSE_KEY`
+new_relic_api_key = `cat #{env_dir}/NEW_RELIC_API_KEY`
 new_relic_app_id = `cat #{env_dir}/NEW_RELIC_APP_ID`
 
 params = {
@@ -13,11 +13,11 @@ params = {
 }
 
 cmd = <<-CMD.gsub(/\s+/, ' ')
-curl -X POST 'https://api.newrelic.com/v2/applications/#{new_relic_app_id}/deployments.json' 
-     -H 'X-Api-Key:#{new_relic_api_key}' -i 
-     -H 'Content-Type: application/json' 
+curl -X POST 'https://api.newrelic.com/v2/applications/#{new_relic_app_id}/deployments.json'
+     -H 'X-Api-Key:#{new_relic_api_key}' -i
+     -H 'Content-Type: application/json'
      -s
-     -d 
+     -d
      '#{params.to_json}'
      > /dev/null 2>&1
 CMD
